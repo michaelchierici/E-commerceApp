14fe5d2dde1aeec904af95fad49646ed3b730fe8
Michael Chierici <michael.chierjr@gmail.com>
2022-07-17 11:54:50 -0300

    feat(loading): create a animation loading with lottie

--- src/components/loading/style.ts
+++ src/components/loading/style.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components/native';
 
-export const Title = styled.View``;
+export const RotatedBox = styled.View`
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+export const Animation = styled.View`
+  font-variant: small-caps;
+  margin: 5px 7px 2px;
+`;
